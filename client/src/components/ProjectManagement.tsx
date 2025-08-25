05f0d284500392e785fed6966ab731423ac7ab38
govindk112 <govindkarguppikar112@gmail.com>
2025-08-25 20:31:58 +0530

    bugs

--- client/src/components/ProjectManagement.tsx
+++ client/src/components/ProjectManagement.tsx
@@ -1367,7 +1367,6 @@ export default function ProjectManagement() {
                         <span className="text-xs text-gray-500">({taskCount} tasks)</span>
                       </div>
                       <div className="text-xs text-gray-400 mt-1">
-                        Owner: {ownerName}
                         {isOwner && <span className="text-green-600"> (You)</span>}
                       </div>
                     </CardContent>
